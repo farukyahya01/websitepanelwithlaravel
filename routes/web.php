31136bc1d44f160924ec746b473201f21549a996
farukyahya01 <ifarukaksu@gmail.com>
2022-03-15 00:25:54 +0300

    install all package all settings are configured

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::group(
+    [
+        'prefix' => LaravelLocalization::setLocale(),
+        'middleware' => [ 'localize','localeSessionRedirect', 'localeViewPath' ]
+    ], function(){ 
+        Route::get('/', [Controllers\SiteController::class, 'index'])->name('index');
+    });
+
+
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
