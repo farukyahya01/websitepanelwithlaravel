a1e70042d0a0972402c3144e677848e506480c52
farukyahya01 <ifarukaksu@gmail.com>
2022-03-17 01:25:37 +0300

    complete gallery - reference section

---
+++ app/Models/ReferenceTranslation.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ReferenceTranslation extends Model
+{
+    use HasFactory;
+    protected $table = 'references_translations';
+}
