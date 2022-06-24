84cc9861e4b035240cbbd9159dee1681c56dbbcf
farukyahya01 <ifarukaksu@gmail.com>
2022-06-24 20:48:23 +0300

    *upload project

---
+++ app/Models/AboutSectionTranslation.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AboutSectionTranslation extends Model
+{
+    use HasFactory;
+    protected $table = 'about_section_translations';
+}
